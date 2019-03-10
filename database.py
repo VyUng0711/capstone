6659872bbdf9edb8f4adac3447687593c2b9c842
Vy Ung <vyung.venus@gmail.com>
2019-03-10 14:33:19 -0700

    Small fixes

--- database.py
+++ database.py
@@ -8,12 +8,8 @@ db_file_location = "database_file/all_tables.db"
 def list_users():
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
-
     _c.execute("select username from users;")
     result = [x[0] for x in _c.fetchall()]
-
-    # _c.close()
     _conn.close()
     
     return result
@@ -22,12 +18,8 @@ def list_users():
 def verify(id, pw):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
-
     _c.execute("select password from users where username = '" + id + "';")
     result = _c.fetchone()[0] == hashlib.sha256(pw.encode()).hexdigest()
-
-    # _c.close()
     _conn.close()
 
     return result
@@ -36,43 +28,29 @@ def verify(id, pw):
 def delete_user_from_db(id):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-
-    # _c = get_db()
-
     _c.execute("delete from users where username = '" + id + "';")
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
 
     # when we delete a user from database USERS, we also need to delete all his or her notes data from database NOTES
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
     _c.execute("delete from notes where user_id in (select id from users where username = '" + id + "');")
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
+
     # when we delete a user from database USERS, we also need to delete all his or her queries data from database QUERIES
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
     _c.execute("delete from queries where user_id in (select id from users where username = '" + id + "');")
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
 
 
 def add_user_into_db(id, pw):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
-
     _c.execute("insert into users (username, password) values(?, ?)", (id.upper(), hashlib.sha256(pw.encode()).hexdigest()))
-    # _c.commit()
-    # _c.close()
     _conn.commit()
     _conn.close()
 
@@ -80,41 +58,33 @@ def add_user_into_db(id, pw):
 def read_query_from_db(id):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
     command = "select q.query_id, q.time_stamp, q.query from queries q join users u on q.user_id = u.id where u.username = '" + id.upper() + "';"
     _c.execute(command)
     result = _c.fetchall()
-    #
     _conn.commit()
     _conn.close()
 
-    # _c.commit()
-    # _c.close()
     return result
 
 
 def read_note_from_db(id):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
-
     command = "select n.note_id, n.time_stamp, note from notes n join users u on n.user_id = u.id where u.username = '" + id.upper() + "';"
     _c.execute(command)
     result = _c.fetchall()
 
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
 
     return result
 
 
 def match_user_id_with_note_id(note_id):
-    # Given the note id, confirm if the current user is the owner of the note which is being operated.
+    # Given the note id, get username
+    # to later confirm if the current user is the owner of the note which is being operated.
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
 
     command = "select u.username from notes n join users u on n.user_id = u.id where n.note_id = '" + note_id + "';"
     _c.execute(command)
@@ -122,15 +92,13 @@ def match_user_id_with_note_id(note_id):
 
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
+
     return result
 
 
 def write_query_into_db(id, query_to_write):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
     command = "select id from users where username = '" + id.upper() + "';"
     _c.execute(command)
     user_id = _c.fetchone()[0]
@@ -148,14 +116,11 @@ def write_query_into_db(id, query_to_write):
     ))
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
 
 
 def write_note_into_db(id, note_to_write):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
-    # _c = get_db()
     command = "select id from users where username = '" + id.upper() + "';"
     _c.execute(command)
     user_id = _c.fetchone()[0]
@@ -173,22 +138,17 @@ def write_note_into_db(id, note_to_write):
     ))
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
 
 
 def delete_note_from_db(note_id):
     _conn = sqlite3.connect(db_file_location)
     _c = _conn.cursor()
 
-    # _c = get_db()
     command = "delete from notes where note_id = '" + note_id + "';"
     _c.execute(command)
 
     _conn.commit()
     _conn.close()
-    # _c.commit()
-    # _c.close()
 
 
 if __name__ == "__main__":
